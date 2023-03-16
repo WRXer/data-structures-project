800ed3138d6177dfd00d21c84f8406f991bdef38
WRXer <king_311@mail.ru>
2023-03-16 17:49:39 +0400

    Переписан репр и стр

--- src/queue.py
+++ src/queue.py
@@ -17,20 +17,33 @@ class Queue:
     def __init__(self):
         """Конструктор класса Queue"""
         self.head = None
-        self.queue = str()
         self.tail = None
 
     def __repr__(self):
         """
         Магический метод для отображения информации об объекте класса в режиме отладки
         """
-        return self.queue[:-1]
+        result = []
+        if self.head is None:
+            return ""
+        head = self.head
+        while head:
+            result.append(head.data)
+            head = head.next_node
+        return '\n'.join(result)
 
     def __str__(self):
         """
         Магический метод для строкового представления объекта пользователю
         """
-        return self.queue[:-1]
+        result = []
+        if self.head is None:
+            return ""
+        head = self.head
+        while head:
+            result.append(head.data)
+            head = head.next_node
+        return '\n'.join(result)
 
     def enqueue(self, data):
         """
@@ -43,13 +56,11 @@ class Queue:
         if self.head == None:
             self.head = node    # если список пуст, то новый узел идет первым
             self.tail = self.head    # находим последний узел в списке
-            self.queue += f"{self.head.data}\n"
         else:
             self.tail.next_node = node    # добавляем новый узел
             self.tail.next_node.next_node = self.head
             self.tail = self.tail.next_node
             self.tail.next_node = None
-            self.queue += f"{self.tail.data}\n"
 
     def dequeue(self, data):
         """

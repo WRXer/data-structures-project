1dd2799c8da3cba4bb2937a8e1b352c71aac910b
WRXer <king_311@mail.ru>
2023-03-25 19:26:53 +0400

    Написан класс Node и LinkedList с функциями приема данных в начало и конец Исправлена функция вывода данных, не проходило проверку из-за пробела в начале строки

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,43 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        """Конструктор класса"""
+        self.head = None
+        self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        node = Node(data, None)
+        if self.head == None:
+            self.head = node
+            self.tail = self.head
+        else:
+            node.next_node = self.head
+            self.head = node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        node = Node(data, None)
+        if self.head == None:
+            self.head = node
+            self.tail = self.head
+        else:
+            self.tail.next_node = node
+            self.tail = node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -20,10 +45,16 @@ class LinkedList:
         if node is None:
             return str(None)
 
+        counter = 0
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
-            node = node.next_node
+            if counter == 0:
+                ll_string += f'{str(node.data)} ->'
+                node = node.next_node
+                counter += 1
+            else:
+                ll_string += f' {str(node.data)} ->'
+                node = node.next_node
 
         ll_string += ' None'
         return ll_string
